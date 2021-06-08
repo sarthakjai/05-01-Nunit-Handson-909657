83fb1275859dbfbc1b4bf24fe9924718c30b17f0
sarthakjai <sarthikjain.sj@outlook.com>
2021-06-08 13:21:34 +0530

    program file

---
+++ CalcLibrary.test/CalculatorTests.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace CalcLibrary.test
+{
+    [TestFixture]
+    class CalculatorTests
+    {
+        SimpleCalculator calculator = null;
+
+        [SetUp]
+        public void setUp()
+        {
+            calculator = new SimpleCalculator();
+        }
+
+        [TestCase(10, 20, ExpectedResult = 30)]
+        public double TestAddition(double a, double b)
+        {
+            return calculator.Addition(a, b);
+        }
+        [TestCase(100, 50, ExpectedResult = 50)]
+        public double TestSubtraction(double a, double b)
+        {
+            return calculator.Subtraction(a, b);
+        }
+        [TestCase(25, 100, ExpectedResult = 2500)]
+        public double TestMultiplication(double a, double b)
+        {
+            return calculator.Multiplication(a, b);
+        }
+        [TestCase(500, 100, ExpectedResult = 5)]
+        public double TestDivision(double a, double b)
+        {
+            return calculator.Division(a, b);
+        }
+
+        [TearDown]
+        public void cleanUp()
+        {
+            calculator = null;
+        }
+    }
+}
---
+++ CalcLibrary.test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CalcLibrary.test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CalcLibrary.test")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("d6c9b671-b2ec-4de3-92bb-ae8c2bb28c08")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
